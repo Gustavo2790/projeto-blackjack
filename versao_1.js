e9489248648b03d2c090095b859413e77bf47d0b
Gustavo <gustavosoares2790@gmail.com>
2023-09-16 13:06:46 -0300

    Blackjack versão 1.0

--- versao_1.js
+++ versao_1.js
@@ -1,12 +1,32 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+
+console.log("Boas vindas ao jogo de BlackJack!") 
+ 
+comprarCarta()
+            
+if (confirm("Quer iniciar uma nova rodada?")) {
+   let carta1Usuario = comprarCarta()
+   let carta2Usuario = comprarCarta()
+   let valorUsuario = carta1Usuario.valor + carta2Usuario.valor
+   console.log(`Usuário - cartas: ${carta1Usuario.texto} ${carta2Usuario.texto} - pontuação: ${valorUsuario}`)
+   let carta1Computador = comprarCarta()
+   let carta2Computador = comprarCarta()
+   let valorComputador = carta1Computador.valor + carta2Computador.valor
+   console.log(`Computador - cartas: ${carta1Computador.texto} ${carta2Computador.texto} - potuação: ${valorComputador}`)
+   if ((valorUsuario <= 21) && (valorUsuario > valorComputador)) {
+      console.log("O usuário ganhou!")
+   } else if (valorUsuario === valorComputador){
+      console.log("Empate!")
+   } else {
+      console.log("O computador ganhou!")
+   }
+
+} else {
+   console.log("O jogo acabou!")
+}
+
+
+ 
+ 
+ 
+ 
+
